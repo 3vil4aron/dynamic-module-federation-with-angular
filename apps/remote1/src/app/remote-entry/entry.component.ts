6204f3bb487d9760b13620db91679fba1ad9072b
Aaron O'Callaghan 🌲 <aaronoc@microsoft.com>
2022-06-22 22:18:27 -0700

    2nd visual

--- apps/remote1/src/app/remote-entry/entry.component.ts
+++ apps/remote1/src/app/remote-entry/entry.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { NgrxFacade } from '@ng-mfe/shared/ngrx';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ng-mfe-remote1-entry',
-  template: `<button (click)="sub()">Sub</button>`,
+  template: `<button (click)="sub()">Sub</button> {{ count$ | async }}`,
 })
 export class RemoteEntryComponent implements OnInit {
-  constructor(private fa: NgrxFacade){}
+  count$: Observable<number>;
+  constructor(private fa: NgrxFacade){
+    this.count$ = this.fa.count$;
+  }
   ngOnInit(): void {
     this.fa.init();
   }
